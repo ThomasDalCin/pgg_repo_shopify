bc7564be2fe2395bb9df6bcbc0b819baf21ecb9b
Thomas Dal Cin <tdalcin96@gmail.com>
2024-06-26 10:23:59 +0200

    changes

--- assets/index.js
+++ assets/index.js
@@ -85,3 +85,181 @@ var swiper = new Swiper(".mySwiper", {
         }
     }
 });
+
+//gsap animation on scroll
+gsap.registerPlugin(ScrollTrigger);
+const mm = gsap.matchMedia();
+const myTranslateTrigger = document.getElementById('translate-effect');
+const myTranslateImg = document.getElementById('translate-img');
+const mySecondTranslateTrigger = document.getElementById('translate-second-effect');
+const mySecondTranslateImg = document.getElementById('translate-second-img');
+const myTranslateAbsolute = document.getElementById('translate-absolute');
+const myAbsoluteImage = document.getElementById('absolute-img');
+const sliderTrigger = document.getElementById('slider-section');
+const sliderItems = document.querySelectorAll('.swiper-slide');
+
+
+mm.add("(max-width: 426px)", () => {
+
+    gsap.fromTo(myTranslateImg, {
+        y: -40
+    }, {
+        y: 0,
+        duration: 1,
+        scrollTrigger: {
+            trigger: myTranslateTrigger,
+            start: "top bottom",
+            end: "bottom top",
+            scrub: true
+        }
+    });
+
+    gsap.fromTo(mySecondTranslateImg, {
+        y: -40
+    }, {
+        y: 0,
+        duration: 1,
+        scrollTrigger: {
+            trigger: mySecondTranslateTrigger,
+            start: "top bottom",
+            end: "bottom top",
+            scrub: true
+        }
+    });
+
+
+    gsap.fromTo(myAbsoluteImage, {
+        y: 0
+    }, {
+        y: 300,
+        duration: 1,
+        ease: 'power1.inOut',
+        scrollTrigger: {
+            trigger: myTranslateAbsolute,
+            start: "top bottom",
+            end: "bottom top",
+            scrub: true
+        }
+    });
+
+    sliderItems.forEach((item, index) => {
+        gsap.to(item, {
+            opacity: 1,
+            duration: 1,
+            ease: 'power1.inOut',
+            scrollTrigger: {
+                trigger: sliderTrigger,
+                start: "top-=300px bottom",
+                end: "center center",
+                scrub: true,
+                delay: index * 0.2,
+                once: true
+            }
+        })
+    })
+});
+
+
+mm.add("(min-width: 768px)", () => {
+
+    gsap.fromTo(myTranslateImg, {
+        y: -100
+    }, {
+        y: 0,
+        duration: 1,
+        scrollTrigger: {
+            trigger: myTranslateTrigger,
+            start: "top bottom",
+            end: "bottom top",
+            scrub: true
+        }
+    });
+
+    gsap.fromTo(mySecondTranslateImg, {
+        y: -50
+    }, {
+        y: 0,
+        duration: 1,
+        scrollTrigger: {
+            trigger: mySecondTranslateTrigger,
+            start: "top bottom",
+            end: "bottom top",
+            scrub: true
+        }
+    });
+
+
+    gsap.fromTo(myAbsoluteImage, {
+        y: 0
+    }, {
+        y: 300,
+        duration: 1,
+        ease: 'power1.inOut',
+        scrollTrigger: {
+            trigger: myTranslateAbsolute,
+            start: "top bottom",
+            end: "bottom top",
+            scrub: true
+        }
+    });
+
+    sliderItems.forEach((item, index) => {
+        gsap.fromTo(item, {
+            y: 200 + (index * 1000),
+        }, {
+            y: 0,
+            duration: 1,
+            ease: 'power1.inOut',
+            scrollTrigger: {
+                trigger: sliderTrigger,
+                start: "top-=300px bottom",
+                end: "center center",
+                scrub: true,
+                delay: index * 0.2,
+                once: true
+            }
+        })
+    })
+});
+
+mm.add("(min-width: 427px) and (max-width: 435px)", () => {
+    gsap.fromTo(myTranslateImg, {
+        y: -60
+    }, {
+        y: 0,
+        duration: 1,
+        scrollTrigger: {
+            trigger: myTranslateTrigger,
+            start: "top bottom",
+            end: "bottom top",
+            scrub: true
+        }
+    });
+
+    gsap.fromTo(mySecondTranslateImg, {
+        y: -60
+    }, {
+        y: 0,
+        duration: 1,
+        scrollTrigger: {
+            trigger: mySecondTranslateTrigger,
+            start: "top bottom",
+            end: "bottom top",
+            scrub: true
+        }
+    });
+
+    gsap.fromTo(myAbsoluteImage, {
+        y: -100
+    }, {
+        y: 100,
+        duration: 1,
+        ease: 'power1.inOut',
+        scrollTrigger: {
+            trigger: myTranslateAbsolute,
+            start: "center center",
+            end: "bottom center",
+            scrub: true
+        }
+    });
+});
