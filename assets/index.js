33e1e0c12d7b491365eb16a1060291c69e532809
Thomas Dal Cin <tdalcin96@gmail.com>
2024-06-26 10:53:20 +0200

    changes

--- assets/index.js
+++ assets/index.js
@@ -53,38 +53,6 @@ function handleMenu() {
 }
 
 
-//settings slider start
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 24,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        color: "black"
-    },
-    breakpoints: {
-
-        320: {
-            slidesPerView: 1,
-            spaceBetween: 16,
-        },
-
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-        },
-
-        1024: {
-            slidesPerView: 2,
-            spaceBetween: 24,
-        },
-
-        1200: {
-            slidesPerView: 3,
-            spaceBetween: 24,
-        }
-    }
-});
 
 //gsap animation on scroll
 gsap.registerPlugin(ScrollTrigger);
